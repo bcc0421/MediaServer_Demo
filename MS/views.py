14f51d1adadb91f924c1271b647601b18a321b0a
Robin <robin@ubuntu.(none)>
2013-09-10 08:50:05 +0800

    [MS] add database connection.

--- MS/views.py
+++ MS/views.py
@@ -1,11 +1,11 @@
 # Create your views here.
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render_to_response
 
 
 def index(request):
     media_file = 'toystory3.flv'
     rtmp_path = 'rtmp://localhost/oflaDemo'
-    return render(request, 'index.html', {
+    return render_to_response('index.html', {
         'media_file': media_file,
         'rtmp_path': rtmp_path
     })
--- MediaServer/settings.py
+++ MediaServer/settings.py
@@ -12,13 +12,19 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '', # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.',
+        'NAME': '',
         'USER': '',
         'PASSWORD': '',
-        'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '', # Set to empty string for default.
+        'HOST': '',
+        'PORT': '',
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'MediaServer_Demo',
+        # 'USER': 'root',
+        # 'PASSWORD': 'root',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
     }
 }
 
